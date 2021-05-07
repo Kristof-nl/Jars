72c5c5cfa8a7cef0f93d43fbac2143544b784aa2
Krzysztof <hermaszewski.krzysztof@gmail.com>
2021-05-08 00:00:00 +0200

    Add amount

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Jars
+Create Jars with currency attached
+Deposit and withdraw resources from jar
+Transfer resources between jars
+Display and sort history of jar operations
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -31,9 +31,32 @@ class JarSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'amount')
 
+
 #Initialize schema
 jar_schema = JarSchema()
 jars_schema = JarSchema(many=True)
+
+
+#Operation class model
+class Operation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    jar_name = db.Column(db.String(50), unique=True)
+    sort_operation = db.Column(db.String(50))
+    amount = db.Column(db.Float)
+
+    def __init__(self, jar_name, sort_operation, amount):
+        self.jar_name = jar_name
+        self.sort_operation = sort_operation
+        self.amount = amount
+
+#Operation schema
+class OperationSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'jar_name', 'sort_operation', 'amount')
+
+#Initialize schema
+operation_schema = OperationSchema()
+operations_schema = JarSchema(many=True)
 
 
 #Create a jar
@@ -41,13 +64,15 @@ jars_schema = JarSchema(many=True)
 def add_jar():
     name = request.json['name']
     amount = request.json['amount']
+    if amount < 0:
+        return "Amount can't be smaller than 0"
+    else:
+        new_jar = Jar(name, amount)
 
-    new_jar = Jar(name, amount)
+        db.session.add(new_jar)
+        db.session.commit()
 
-    db.session.add(new_jar)
-    db.session.commit()
-
-    return jar_schema.jsonify(new_jar)
+        return jar_schema.jsonify(new_jar)
 
 
 #Get all jars
@@ -87,6 +112,19 @@ def delete_jar(id):
     jar = Jar.query.get(id)
 
     db.session.delete(jar)
+    db.session.commit()
+
+    return f"Choosen jar id: {id} is deleted"
+
+#Add amount
+@app.route('/jar/<id>/add', methods=['PUT'])
+def add_to_jar(id):
+    jar = Jar.query.get(id)
+    old_amount = jar.amount
+    from_form = request.form
+    
+    jar.amount = float(old_amount) + float(from_form['amount'])
+    
     db.session.commit()
 
     return jar_schema.jsonify(jar)
